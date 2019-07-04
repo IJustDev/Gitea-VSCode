011232bc7593bd2f7415624dfcf5d16191992a00
ljoonal <ljoonal@ljoonal.xyz>
2019-07-04 15:45:53 +0300

    A bit of cleanup

--- src/template.html.ts
+++ src/template.html.ts
@@ -1,38 +1,42 @@
-import { Issue } from "./issue";
+import { Issue } from './issue';
 
 export function showIssueHTML(issue: Issue) {
-
-    return `<body>
+  return `<body>
     <h1>{{label}}</h1>
-        <table>
-            <tr>
-            <td>
-            Title
-            </td>
-            <td >
-            {{label}}
-            </td>
-            </tr>
-            <tr>
-            <td>
-            State
-            </td>
-            <td>
-            {{state}}
-            </td>
-            </tr>
-            <tr >
-            <td>
-            Assignee
-            </td>
-            <td>
-            {{assignee}}
-            </td>
-            </tr>
-            </table>
-            <p style="font-size: 20pt">
-            {{description}}
-            </p>
-            </body>
-`.replace("{{label}}", issue.label).replace("{{state}}", issue.issueState).replace("{{assignee}}", issue.assignee).replace("{{description}}", issue.body).replace("{{label}}", issue.label);
+    <table>
+      <tr>
+        <td>
+          Title
+        </td>
+        <td >
+          {{label}}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          State
+        </td>
+        <td>
+          {{state}}
+        </td>
+      </tr>
+      <tr >
+        <td>
+          Assignee
+        </td>
+        <td>
+          {{assignee}}
+        </td>
+      </tr>
+    </table>
+    <p style="font-size: 20pt">
+      {{description}}
+    </p>
+    </body>
+  `
+    .replace('{{label}}', issue.label)
+    .replace('{{state}}', issue.issueState)
+    .replace('{{assignee}}', issue.assignee)
+    .replace('{{description}}', issue.body)
+    .replace('{{label}}', issue.label);
 }
